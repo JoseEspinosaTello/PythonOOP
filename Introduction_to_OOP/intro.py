dd090e2efc9570d87855a60d7310ae71d1b67724
JoseEspinosaTello <jet5588@outlook.com>
2023-10-23 11:12:34 -0500

    update

---
+++ Introduction_to_OOP/intro.py
@@ -0,0 +1,14 @@
+item1 = 'phone'
+item1_price = 100
+item_quantity = 5
+item1_price_total = item1_price * item_quantity
+
+
+print(type(item1)) #str
+print(type(item1_price)) #int
+print(type(item_quantity)) #int
+print(type(item1_price_total)) #int
+
+#when run for each of the types we also see the keyword class
+#this means that these datatypes are actually instances of strings or integers
+#in python each datatype is an object that has been instatiated earlier by a class
